34ad4bbf194a84bee9db74d22a5c1b265852b0ad
Your Name <david@futuresbright.com>
2026-05-29 17:25:47 +0200

    working on solar systems

--- solar_system.gd
+++ solar_system.gd
@@ -1,6 +1,12 @@
 class_name SolarSystem
 extends Node2D
 
+var MIN_SIZE = 20
+var MAX_SIZE = 50
+
+var MIN_DISTANCE = 100
+var MAX_DISTANCE = 500
+
 var planets = []
 
 func _ready():
@@ -10,13 +16,15 @@ func _ready():
     queue_redraw()
 
 func _draw():
+    draw_circle(Vector2.ZERO, 100, Color.YELLOW)
+
     for planet in planets:
         draw_circle(planet.pos, planet.size, planet.color)
 
 func add_planet():
     var planet = {
-        "pos": Vector2(randi_range(-500, 500), randi_range(-500, 500)),
-        "size": randi_range(100, 500),
+        "pos": Vector2(randi_range(MIN_DISTANCE, MAX_DISTANCE), randi_range(MIN_DISTANCE / 3, MAX_DISTANCE / 3)),
+        "size": randi_range(MIN_SIZE, MAX_SIZE),
         "color": Color.RED
     }
     planets.append(planet)
